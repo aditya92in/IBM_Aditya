517143121e3ae136c3eb1aa77df77cfe9eaed604
aditya92in <aditya92in@gmail.com>
2017-05-05 21:45:44 +0200

    added code

---
+++ Pressure_Correction/explicit/correction.m
@@ -0,0 +1,27 @@
+function [unew,vnew]= correction(u_d_star,v_d_star,pcorr,dt,dx,dy)
+
+    %unew=u_star;
+    unew=u_d_star;
+    vnew=v_d_star;
+%             vnew=v_star;
+           unew(2:end-1,2:end-1)=u_d_star(2:end-1,2:end-1)-(pcorr(2:end-1,3:end-1)-pcorr(2:end-1,2:end-2)).*(dt/dx);
+           vnew(2:end-1,2:end-1)=v_d_star(2:end-1,2:end-1)-(pcorr(3:end-1,2:end-1)-pcorr(2:end-2,2:end-1)).*(dt/dy); 
+%             
+%            
+
+
+%unew(2:end-1,2:end-1)=u_d_star(2:end-1,2:end-1)+fnew_x(2:end-1,2:end-1).*dt;
+%vnew(2:end-1,2:end-1)=v_d_star(2:end-1,2:end-1)+fnew_y(2:end-1,2:end-1).*dt;
+
+%boundary conditions
+unew(1,:)=-unew(2,:);
+unew(end,:)=-unew(end-1,:);
+
+%boundary conditions
+ vnew(1,:)=0;          
+ vnew(end,:)=0;
+ vnew(:,1)=-vnew(:,2);
+ vnew(:,end)=vnew(:,end-1);
+
+            
+end
---
+++ Pressure_Correction/explicit/grid_plotting.m
@@ -0,0 +1,46 @@
+function dummy=grid_plotting(x_c,y_c,imax,jmax,x,y,r)
+
+
+% for i=1:jmax+2 %jmax+2 (number of pressure nodes, including ghost) + jmax+1 (plus number of u-velocity nodes)
+%     for j=1:imax+2 %imax+2 (number of pressure nodes, including ghost)+ imax+1 (plus number of v-velocity nodes) 
+%         plot(x(j,i),y(j,i),'k.','MarkerSize',10);
+%         hold all
+%     end
+% end
+
+% hold on
+% a=[0 0];
+% b=[0 1];
+% plot(a,b,'b--');
+% a=[0 1];
+% b=[1 1];
+% hold on
+% plot(a,b,'b--');
+% a=[1 1];
+% b=[1 0];
+% plot(a,b,'b--');
+% a=[1 0];
+% b=[0 0];
+% plot(a,b,'b--');
+
+
+
+
+% plotting points for u
+%  for i=2:2:2*(jmax)+3  %outer loop traverses across x (the index name 'i' does not matter)
+%       for j=1:2:(2*imax)+3 %inner loop traverses across y
+%           plot(x(j,i),y(j,i),'x','MarkerSize',10);
+%           hold all
+%       end
+%   end
+%  plotting points for v
+%  for i=1:2:(2*jmax)+3
+%      for j=2:2:(2*imax)+3
+%          plot(x(j,i),y(j,i),'o','MarkerSize',10);
+%          hold all
+%      end
+% end
+
+circle(x_c,y_c,r)
+
+end
