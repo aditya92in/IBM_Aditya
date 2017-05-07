e9388cbc9325d86ed142cc2384068e769dc80002
aditya92in <aditya92in@gmail.com>
2017-05-07 20:06:29 +0200

    Delete viscous_coeff.m

--- Navier_Stokes_Test_Case/Lid_Driven_Cavity/viscous_coeff.m
+++
@@ -1,6 +0,0 @@
-function [du_e,du_w,du_s,du_n]=viscous_coeff(uold,dx,dy)
-du_e=(uold(2:end-1,3:end)-uold(2:end-1,2:end-1))/(dx);
-du_w=(uold(2:end-1,2:end-1)-uold(2:end-1,2:end-1))/(dx);
-du_s=(uold(2:end-1,2:end-1)-uold(1:end-2,2:end-1))/(dy);
-du_n=(uold(3:end,2:end-1)-uold(2:end-1,2:end-1))/(dy);
-end
