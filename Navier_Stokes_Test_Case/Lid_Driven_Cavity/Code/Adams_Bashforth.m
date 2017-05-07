287b92fc61aade1a85b52999901b84c281fc2337
aditya92in <aditya92in@gmail.com>
2017-05-07 23:28:46 +0200

    Update Adams_Bashforth.m

--- Navier_Stokes_Test_Case/Lid_Driven_Cavity/Code/Adams_Bashforth.m
+++ Navier_Stokes_Test_Case/Lid_Driven_Cavity/Code/Adams_Bashforth.m
@@ -5,7 +5,7 @@ if(count_star==1)
 u_star(2:end-1,2:end-1)=uold(2:end-1,2:end-1)-(dt/(dx*dy))*(A_u(2:end-1,2:end-1,count_star))+(dt/(Re*dx*dy))*(D_u(2:end-1,2:end-1,count_star));
 v_star(2:end-1,2:end-1)=vold(2:end-1,2:end-1)-(dt/(dx*dy))*(A_v(2:end-1,2:end-1,count_star))+(dt/(Re*dx*dy))*(D_v(2:end-1,2:end-1,count_star));
 else  
-u_star(2:end-1,2:end-1)=uold(2:end-1,2:end-1)-(dt/(dx*dy))*(1.5*A_u(2:end-1,2:end-1,count_star)-0.5*A_u(2:end-1,2:end-1,count_star-1))+(dt/(dx*dy))*(1.5*D_u(2:end-1,2:end-1,count_star)-0.5*(D_u(2:end-1,2:end-1,count_star-1)));
-v_star(2:end-1,2:end-1)=vold(2:end-1,2:end-1)-(dt/(dx*dy))*(1.5*A_v(2:end-1,2:end-1,count_star)-0.5*A_v(2:end-1,2:end-1,count_star-1))+(dt/(dx*dy))*(1.5*D_v(2:end-1,2:end-1,count_star)-0.5*D_v(2:end-1,2:end-1,count_star-1));
+u_star(2:end-1,2:end-1)=uold(2:end-1,2:end-1)-(dt/(dx*dy))*(1.5*A_u(2:end-1,2:end-1,count_star)-0.5*A_u(2:end-1,2:end-1,count_star-1))+(dt/(Re*dx*dy))*(1.5*D_u(2:end-1,2:end-1,count_star)-0.5*(D_u(2:end-1,2:end-1,count_star-1)));
+v_star(2:end-1,2:end-1)=vold(2:end-1,2:end-1)-(dt/(dx*dy))*(1.5*A_v(2:end-1,2:end-1,count_star)-0.5*A_v(2:end-1,2:end-1,count_star-1))+(dt/(Re*dx*dy))*(1.5*D_v(2:end-1,2:end-1,count_star)-0.5*D_v(2:end-1,2:end-1,count_star-1));
 
 end
