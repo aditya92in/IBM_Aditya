0951756aa975f797bd970ed8162c96c62e589431
aditya92in <aditya92in@gmail.com>
2017-05-08 01:01:53 +0200

    Delete b_c_v.m

--- Navier_Stokes_FV/b_c_v.m
+++
@@ -1,6 +0,0 @@
-function vold=b_c_v(vold,V_w,V_e,V_s,V_n)
-vold(2:end-1,2)=2*V_w-vold(2:end-1,1); %V_w=0;
-vold(2:end-1,end-1)=vold(2:end-1,end); %dv_e/dx=0;
-vold(1,2:end-1)=V_s; %V_s=0; no penetration south
-vold(2:end-1,end)=V_n; %V_n=0; no penetration north
-end
