897931fd44948b18cd42b8b3cd8f40d527a313b2
aditya92in <aditya92in@gmail.com>
2017-05-08 23:41:20 +0200

    Update b_c_v.m

--- Navier_Stokes_Test_Case/Flow_between_plates/Code/b_c_v.m
+++ Navier_Stokes_Test_Case/Flow_between_plates/Code/b_c_v.m
@@ -2,5 +2,5 @@ function vold=b_c_v(vold,V_w,V_e,V_s,V_n)
 vold(2:end-1,1)=2*V_w-vold(2:end-1,2); %V_w=0;
 vold(2:end-1,end)=vold(2:end-1,end-1); %dv_e/dx=0;
 vold(1,2:end-1)=V_s; %V_s=0; no penetration south
-vold(2:end-1,end)=V_n; %V_n=0; no penetration north
+vold(end,2:end-1)=V_n; %V_n=0; no penetration north
 end
